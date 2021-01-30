4b3e6eed54139bbbaa116f70087c67b0995e9a89
Tan Nian Wei <tannianwei@aggienetwork.com>
2021-01-30 14:58:23 +0800

    docs: updated README w recent developments

--- README.md
+++ README.md
@@ -2,4 +2,22 @@
 
 Generative art with neural networks in Pytorch. Read this [Towards Data Science article](https://towardsdatascience.com/making-deep-neural-networks-paint-to-understand-how-they-work-4be0901582ee) and [this post on otoro.net](https://blog.otoro.net/2015/06/19/neural-network-generative-art/), mesmerized by the wild splotches of colour, and decided to dive into this further. WIP.
 
-What I have atm: ![grid of generated abstract images using neural networks](output/grid.png)
+Replicating work in the blogpost produced this: ![grid of generated abstract images using neural networks](output/grid.png)
+
+Expanded upon it further to generate smoothly varying frames over time, by sampling trajectories in a latent space. Added image upscaling to get this to run on my Thinkpad X220. Example in `cli.py`, libraries used in `environment.yml`.
+
+![Animated latent walk](output/animated-latent-walk.gif)
+
+``` bash
+(nn-gen-art) tnwei@rama:~/projects/nn-generative-art$ python cli.py -h
+usage: cli.py [-h] [--XDIM XDIM] [--YDIM YDIM] [--scale SCALE] [--noframelimit]
+
+optional arguments:
+  -h, --help      show this help message and exit
+  --XDIM XDIM     Dimension of X, defaults to 160
+  --YDIM YDIM     Dimension of Y, defaults to 90
+  --scale SCALE   Scale factor to resize the image, defaults to 4
+  --noframelimit  Lifts cap on max frames per second, not recommended
+```
+
+
--- cli.py
+++ cli.py
@@ -8,12 +8,37 @@ import cv2
 import time
 from timeit import default_timer as timer
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--XDIM", help="Dimension of X, defaults to 160", type=int, default=160
+)
+parser.add_argument(
+    "--YDIM", help="Dimension of Y, defaults to 90", type=int, default=90
+)
+parser.add_argument(
+    "--scale",
+    help="Scale factor to resize the image, defaults to 4",
+    type=float,
+    default=4,
+)
+parser.add_argument(
+    "--noframelimit",
+    help="Lifts cap on max frames per second, not recommended",
+    action="store_true",
+)
+args = parser.parse_args()
 
 if __name__ == "__main__":
-
+    XDIM, YDIM = args.XDIM, args.YDIM
     lss = LatentSpaceSampler(min_coord=[-2, -2, -2], max_coord=[2, 2, 2], stepsize=0.05)
     net = Net(num_hidden_layers=2, num_neurons=64)
-    max_fps = 30
+    max_fps = 24
+    max_frame_duration = 1 / max_fps
+
+    if args.noframelimit is False:
+        print(f"Frame limit set at {max_fps} FPS. Use --noframelimit to disable")
 
     # Format output window
     # https://stackoverflow.com/a/49095781
@@ -28,19 +53,28 @@ if __name__ == "__main__":
             # Was going to just be `while True`
             # But OpenCV windows don't stay closed when you close them in the GUI!
             # Explicitly checking that the window is closed to end the loop
+            frame_start = timer()
 
             out = generate_one_art(
                 net,
                 latent_vec=lss.sample(),
-                input_config={"img_width": 640, "img_height": 640},
+                input_config={"img_width": XDIM, "img_height": YDIM},
             )
-
-            # Already averages < 1.5 FPS on my laptop
-            # But including an FPS cap here for fast machines
-            time.sleep(1 / max_fps)
 
             # Convert to BGR
             out = cv2.cvtColor(out, cv2.COLOR_RGB2BGR)
+
+            # Upscale
+            if args.scale is not None:
+                out = cv2.resize(
+                    src=out,
+                    dsize=(0, 0),
+                    fx=args.scale,
+                    fy=args.scale,
+                    interpolation=cv2.INTER_LINEAR,
+                )
+
+            # Display
             cv2.imshow(window_name, out)
 
             if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -55,6 +89,14 @@ if __name__ == "__main__":
                     stopwatch = timer()
 
             frame_count += 1
+
+            # Cap FPS to 24
+            frame_end = timer()
+
+            if args.noframelimit is False:
+                time_passed = frame_end - frame_start
+                if time_passed < max_frame_duration:
+                    time.sleep(max_frame_duration - time_passed)
 
         # After breaking while loop
         cv2.destroyAllWindows()
