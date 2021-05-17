c3cd6a7b316e69934332379e018b0954e4fa38a8
Tan Nian Wei <tannianwei@aggienetwork.com>
2021-05-17 23:12:59 +0800

    refactor: improved linear sampler code readability

--- code/sampler.py
+++ code/sampler.py
@@ -12,6 +12,73 @@ except:
     from bezier import gen_bezier_ctrl_points, bezier_mat
 
 
+def linear_sampler(
+    dims: int,
+    min_coord: np.ndarray,
+    max_coord: np.ndarray,
+    init_coord: np.ndarray,
+    stepsize: float,
+    smooth_start_stop: bool,
+) -> Generator[np.ndarray, None, None]:
+    current_point = init_coord
+    while True:
+        # Find next point
+        next_point = np.random.uniform(low=min_coord, high=max_coord, size=(dims,))
+
+        # Calculate distance because we want to determine steps and direction
+        dist = np.linalg.norm(next_point - current_point)
+        stepcount = int(dist / stepsize)
+        steps = np.linspace(current_point, next_point, stepcount)
+
+        # Smooth decel and accel
+        if smooth_start_stop is True:
+            # Decel final 20% of path
+            # 80% to 90% runs at 1.5x steps
+            # 90% to 100% runs at 3x steps
+            decel_point1 = int(stepcount * 0.8)
+            decel_point2 = int(stepcount * 0.9)
+            decel_path1 = np.linspace(
+                steps[decel_point1],
+                steps[decel_point2],
+                int(1.5 * (decel_point2 - decel_point1)),
+            )
+            decel_path2 = np.linspace(
+                steps[decel_point2], steps[-1], int(3 * (stepcount - decel_point2))
+            )
+
+            # Accel first 10% of path
+            # 0% to 10% stretched into 3x steps
+            # 10% to 20% stretched into 1.5x steps
+            # Pretty much reverse of above
+            accel_point1 = int(stepcount * 0.1)
+            accel_point2 = int(stepcount * 0.2)
+            accel_path1 = np.linspace(
+                steps[0], steps[accel_point1], int(3 * (accel_point1 - 0))
+            )
+            accel_path2 = np.linspace(
+                steps[accel_point1],
+                steps[accel_point2],
+                int(1.5 * (accel_point2 - accel_point1)),
+            )
+
+            steps = np.concatenate(
+                [
+                    accel_path1,
+                    accel_path2,
+                    steps[accel_point2:decel_point1],
+                    decel_path1,
+                    decel_path2,
+                ]
+            )
+        else:
+            pass
+
+        for i in range(steps.shape[0]):
+            yield steps[i, :]
+
+        current_point = next_point
+
+
 class LatentSpaceSampler:
     def __init__(
         self,
@@ -49,11 +116,7 @@ class LatentSpaceSampler:
         self.past_latent_vecs = []
 
         # Initialize the generator
-        self._init_generator()
-
-    def _init_generator(self) -> NoReturn:
-        # Writing it this way to leave flexibility for implementing other generators
-        self.generator = self._point2point_latent_explorer_generator(
+        self.generator = linear_sampler(
             dims=self.dims,
             min_coord=self.min_coord,
             max_coord=self.max_coord,
@@ -61,74 +124,6 @@ class LatentSpaceSampler:
             stepsize=self.stepsize,
             smooth_start_stop=self.smooth_start_stop,
         )
-
-    def _point2point_latent_explorer_generator(
-        self,
-        dims: int,
-        min_coord: np.ndarray,
-        max_coord: np.ndarray,
-        init_coord: np.ndarray,
-        stepsize: float,
-        smooth_start_stop: bool,
-    ) -> Generator[np.ndarray, None, None]:
-        current_point = init_coord
-        while True:
-            # Find next point
-            next_point = np.random.uniform(low=min_coord, high=max_coord, size=(dims,))
-
-            # Calculate distance because we want to determine steps and direction
-            dist = np.linalg.norm(next_point - current_point)
-            stepcount = int(dist / stepsize)
-            steps = np.linspace(current_point, next_point, stepcount)
-
-            # Smooth decel and accel
-            if smooth_start_stop is True:
-                # Decel final 20% of path
-                # 80% to 90% runs at 1.5x steps
-                # 90% to 100% runs at 3x steps
-                decel_point1 = int(stepcount * 0.8)
-                decel_point2 = int(stepcount * 0.9)
-                decel_path1 = np.linspace(
-                    steps[decel_point1],
-                    steps[decel_point2],
-                    int(1.5 * (decel_point2 - decel_point1)),
-                )
-                decel_path2 = np.linspace(
-                    steps[decel_point2], steps[-1], int(3 * (stepcount - decel_point2))
-                )
-
-                # Accel first 10% of path
-                # 0% to 10% stretched into 3x steps
-                # 10% to 20% stretched into 1.5x steps
-                # Pretty much reverse of above
-                accel_point1 = int(stepcount * 0.1)
-                accel_point2 = int(stepcount * 0.2)
-                accel_path1 = np.linspace(
-                    steps[0], steps[accel_point1], int(3 * (accel_point1 - 0))
-                )
-                accel_path2 = np.linspace(
-                    steps[accel_point1],
-                    steps[accel_point2],
-                    int(1.5 * (accel_point2 - accel_point1)),
-                )
-
-                steps = np.concatenate(
-                    [
-                        accel_path1,
-                        accel_path2,
-                        steps[accel_point2:decel_point1],
-                        decel_path1,
-                        decel_path2,
-                    ]
-                )
-            else:
-                pass
-
-            for i in range(steps.shape[0]):
-                self.update_latent_vecs(steps[i, :])
-                yield steps[i, :]
-
-            current_point = next_point
 
     def update_latent_vecs(self, latent_vec: np.ndarray) -> NoReturn:
         """
@@ -142,9 +137,7 @@ class LatentSpaceSampler:
             if len(self.past_latent_vecs) > self.keep_past_latent_vecs:
                 self.past_latent_vecs.pop(0)
 
-    def sample(
-        self, iterations: int = 1, return_value_if_one: bool = True
-    ) -> np.ndarray:
+    def sample(self) -> np.ndarray:
         """
         Explore latent space by travelling to a randomly selected point.
         This process is repeated until the number of steps as specified by
@@ -152,23 +145,16 @@ class LatentSpaceSampler:
         returned together with the path lengths. If `iterations` is None, continuously produces frames.
 
         The heavy lifting is done in the generator object created by `_create_generator`, this is simply the sampling function.
-
         """
-        if iterations is None:
-            iterations = 1
-
-        if iterations == 1 and return_value_if_one is True:
-            return next(self.generator)
-
-        ans = []
-
-        for _ in range(iterations):
-            ans.append(next(self.generator))
-
-        return ans
+        next_latent_vec = next(self.generator)
+        self.update_latent_vecs(next_latent_vec)
+        return next_latent_vec
 
 
-def bezier_sampler(num_ctrl_points=5, steps=50):
+# ref type for generator expressions: https://stackoverflow.com/a/42227485/13095028
+def bezier_sampler(
+    num_ctrl_points: int = 5, steps: int = 50
+) -> Generator[np.ndarray, None, None]:
     prev_ctrl_points = None
     while True:
         ctrl_points = gen_bezier_ctrl_points(num_ctrl_points, prev_ctrl_points)
